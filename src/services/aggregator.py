1109a39509c12d961787f7e1e9d9facd5c094864
Lyova PArsyan <parsyan.lyova@gmail.com>
2024-06-13 15:50:12 +0400

    minor change with linter

--- src/services/aggregator.py
+++ src/services/aggregator.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Any
 
 
@@ -32,9 +32,35 @@ async def aggregate_salaries(dt_from: str, dt_upto: str, group_type: str, collec
         cursor = collection.aggregate(pipeline)
         result = await cursor.to_list(length=None)
 
-        dataset = [item['total_salary'] for item in result]
-        labels = [item['_id'] for item in result]
+        # Создание словаря с датами и значениями из результата агрегации
+        salaries_dict = {item['_id']: item['total_salary'] for item in result}
+
+        # Создание списка всех дат в указанном диапазоне
+        current_date = dt_from
+        labels = []
+        dataset = []
+
+        while current_date <= dt_upto:
+            if group_type == "day":
+                formatted_date = current_date.strftime("%Y-%m-%dT00:00:00")
+                labels.append(formatted_date)
+                dataset.append(salaries_dict.get(formatted_date, 0))
+                current_date += timedelta(days=1)
+            elif group_type == "hour":
+                formatted_date = current_date.strftime("%Y-%m-%dT%H:00:00")
+                labels.append(formatted_date)
+                dataset.append(salaries_dict.get(formatted_date, 0))
+                current_date += timedelta(hours=1)
+            elif group_type == "month":
+                formatted_date = current_date.strftime("%Y-%m-01T00:00:00")
+                labels.append(formatted_date)
+                dataset.append(salaries_dict.get(formatted_date, 0))
+                current_date += timedelta(days=1)
+                current_date = (current_date.replace(day=1) + timedelta(days=31)).replace(day=1)
+            else:
+                break
 
         return {"dataset": dataset, "labels": labels}
-    except Exception:
+    except Exception as e:
+        print(f"Error: {e}")
         return {"dataset": [], "labels": []}
